7a63d793704b4dd515c8c156cd7b8f7f7a60e9ab
jfnavarro <jc.fernandez.navarro@gmail.com>
2015-09-03 17:27:09 +0200

    Removed Qt3D deprecated code from windows build script
    Change-Id: Iefb2023189023b7a61d262d36117cd67f3db57c0

--- build_cygwin.sh
+++ build_cygwin.sh
@@ -1,55 +1,38 @@
 #!/bin/bash
 
-# Better settle on a specific commit of qt3d.
-#
-# $ git show -s --format=%ci 8a9723d7420829a2ba0141a9f7d8dc8cd11d8331
-# 2014-09-04 13:01:05 +0200
-# $
-
-qt3d_commit=8a9723d7420829a2ba0141a9f7d8dc8cd11d8331
-
 #  Normally the stclient is built like this:
-#  sh build_cygwin.sh /path/to/stclient/source build_qt3d stclient_production_build
+#  sh build_cygwin.sh /path/to/stclient/source stclient_production_build
 #
 #  If you need to edit the stclient source code you can save compilation time if you run
-#  sh build_cygwin.sh /path/to/stclient/source use_last_built_qt3d stclient_build_only_run_make
+#  sh build_cygwin.sh /path/to/stclient/source stclient_build_only_run_make
 
 set -ex
 
 libjpeg_turbo=`cygpath -w /cygdrive/c/libjpeg-turbo`
 
 function print_usage_and_exit {
-    echo "Usage: build_cygwin.sh path_to_st_client_source [ build_qt3d | use_last_built_qt3d ] [ stclient_production_build | stclient_development_build | stclient_build_only_run_make ] result_dir" >&2
+    echo "Usage: build_cygwin.sh path_to_st_client_source [ stclient_production_build | stclient_development_build | stclient_build_only_run_make ] result_dir" >&2
     exit 1
 }
 
-if [ $# -ne 4 ]; then
+if [ $# -ne 3 ]; then
   print_usage_and_exit
 fi
 
-result_dir=$4
+result_dir=$3
 
-if ! [ $2 = "build_qt3d" -o $2 = "use_last_built_qt3d" ]; then
+if ! [ $2 = "stclient_production_build" -o $2 = "stclient_development_build" -o $2 = "stclient_build_only_run_make" ]; then
   print_usage_and_exit
 fi
 
-if ! [ $3 = "stclient_production_build" -o $3 = "stclient_development_build" -o $3 = "stclient_build_only_run_make" ]; then
-  print_usage_and_exit
-fi
-
-
-if [ ! -d "$4" ]; then
-    echo "dir $4 does not exist" >&2
+if [ ! -d "$3" ]; then
+    echo "dir $3 does not exist" >&2
     print_usage_and_exit
 fi
 
-
 stclient_srcdir="$1"
 
-
-
-
-if [ $3 = "stclient_build_only_run_make" ]; then
+if [ $2 = "stclient_build_only_run_make" ]; then
   stclient_builddir=`ls -dt1 /tmp/stclient.* | head -1`
   if [ -z $stclient_builddir ]; then
     echo "error: could not find the last build dir for the stclient under /tmp/stclient.*"  >&2
@@ -65,39 +48,6 @@ fi
 filepath1=`cygpath -w '/cygdrive/c/Program Files (x86)/Microsoft Visual Studio 12.0/VC/vcvarsall.bat'`
 filepath2=`cygpath -w '/cygdrive/c/Qt/Qt5.5.0/5.5/msvc2013_64/bin/qtenv2.bat'`
 
-qt3d_dir=/home/Administrator/qt3d
-qt3d_srcdir=$qt3d_dir/src
-qt3d_builddir=$qt3d_dir/build
-qt3d_srcdir_windows=`cygpath -w $qt3d_srcdir`
-qt3d_builddir_windows=`cygpath -w $qt3d_builddir`
-
-if [ $2 = "build_qt3d" ]; then
-  if [ -d "$qt3d_dir" ]; then
-    rm -rf "$qt3d_dir"
-  fi
-  mkdir -p "$qt3d_dir"
-  mkdir -p "$qt3d_srcdir"
-  mkdir -p "$qt3d_builddir"
-
-  git clone git://gitorious.org/qt/qt3d.git $qt3d_srcdir
-  cd $qt3d_srcdir
-  git checkout $qt3d_commit -b tmpbranch
-
-  # Circumventing an include problem. I guess we run into problems because we are running qmake and MSVC from within
-  # Cygwin.
-
-  echo "INCLUDEPATH += \$\$PWD/dummy" >> src/imports/threed/threed.pro
-  mkdir src/imports/threed/dummy
-  cmd /Q /C call "$filepath1" x86_amd64 "&&" \
-  "$filepath2" "&&" \
-  cd "$qt3d_builddir_windows" "&&" \
-  qmake QMAKE_CXXFLAGS+=/MP "..\\src\\qt3d.pro"  "&&" \
-  nmake release "&&" \
-  nmake install 
-  mkdir -p $qt3d_builddir/bin/
-  cp $qt3d_builddir/lib/*.dll $qt3d_builddir/bin/
-fi
-
 cd "$stclient_builddir" 
 stclient_builddir_windows=`cygpath -w $stclient_builddir`
 stclient_srcdir_windows=`cygpath -w $stclient_srcdir`
@@ -107,19 +57,20 @@ stclient_srcdir_windows=`cygpath -w $stclient_srcdir`
 
 cmake_path="/cygdrive/c/cmake/bin/cmake.exe"
 cmake_path_windows=`cygpath -w "$cmake_path"`
+
 #"Visual Studio 11 Win64" 
 
-if [ $3 = "stclient_development_build" ]; then
+if [ $2 = "stclient_development_build" ]; then
   server=development
   build_type=Debug
 fi
 
-if [ $3 = "stclient_production_build" ]; then
+if [ $2 = "stclient_production_build" ]; then
   server=production
   build_type=Release
 fi
 
-if [ $3 = "stclient_build_only_run_make" ]; then
+if [ $2 = "stclient_build_only_run_make" ]; then
   cmd="echo skipping running cmake"
 else
   cmd=""
@@ -128,7 +79,7 @@ fi
 cmd /Q /C call "$filepath1" x86_amd64 "&&" \
   "$filepath2" "&&" \
    cd "$stclient_builddir_windows" "&&" \
-   $cmd "$cmake_path_windows" -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=$build_type -DSERVER:STRING=$server "-DCMAKE_PREFIX_PATH=$qt3d_builddir_windows;$libjpeg_turbo" "$stclient_srcdir_windows" "&&" \
+   $cmd "$cmake_path_windows" -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=$build_type -DSERVER:STRING=$server "-DCMAKE_PREFIX_PATH=$libjpeg_turbo" "$stclient_srcdir_windows" "&&" \
    nmake "&&" \
    nmake package
 
