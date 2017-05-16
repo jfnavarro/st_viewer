70249f206420c755750e46948eb94330a3ae1384
jfnavarro <jc.fernandez.navarro@gmail.com>
2017-05-16 11:21:22 +0200

    Added support to find RCpp

---
+++ cmake/FindRInside.cmake
@@ -0,0 +1,44 @@
+# Cmake module to find RInisde
+# - Try to find Rinside
+# Once done, this will define
+#
+#  RINSIDE_FOUND - system has RINSIDE
+#  RINSIDE_INCLUDE_DIR - the RINSIDE include directories
+#  RINSIDE_LIBRARY - link these to use RINSIDE
+# Autor: Omar Andres Zapata Mesa 31/05/2013
+# Mofified by: Jose Fernandez Navarro
+
+set(RINSIDE_PKGCONF_LIBRARY_INCLUDE
+    "/usr/local/include"
+    "/usr/include"
+    "/opt/R/site-library/RInside/include"
+    "/usr/local/lib/R/site-library/RInside/include"
+    "/usr/lib/R/site-library/RInside/include"
+    "${RINSIDE_PATH}/include")
+
+set(RINSIDE_PKGCONF_LIBRARY_DIRS
+    "/usr/local/lib"
+    "/usr/lib"
+    "/opt/R/site-library/RInside/lib"
+    "/usr/local/lib/R/site-library/RInside/lib"
+    "/usr/lib/R/site-library/RInside/lib"
+    "${RINSIDE_PATH}/libs"
+    "${RINSIDE_PATH}/libs/x64"
+    "${RINSIDE_PATH}/libs/i386")
+
+# Find the include dir
+find_path(RINSIDE_INCLUDE_DIR
+  NAMES RInside.h
+  PATHS ${RINSIDE_PKGCONF_LIBRARY_INCLUDE}
+)
+
+# Find the library
+find_library(RINSIDE_LIBRARY NAMES RInside PATHS ${RINSIDE_PKGCONF_LIBRARY_DIRS})
+
+if(RINSIDE_INCLUDE_DIR AND RINSIDE_LIBRARY)
+    set(RINSIDE_FOUND TRUE)
+    mark_as_advanced(RINSIDE_LIBRARY RCPP_LIBRARY)
+else()
+    set(RINSIDE_FOUND FALSE)
+    message(FATAL_ERROR "Looking for RInside -- not found. Try to set RINSIDE_PATH to the path of RInside")
+endif()
--- cmake/FindRcpp.cmake
+++ cmake/FindRcpp.cmake
@@ -1,28 +1,22 @@
-#Cmake module to find Rcpp
+# Cmake module to find Rcpp
 # - Try to find Rcpp
 # Once done, this will define
 #
 #  RCPP_FOUND - system has RCPP
-#  RCPP_INCLUDE_DIRS - the RCPP include directories
-#  RCPP_LIBRARIES - link these to use RCPP
+#  RCPP_INCLUDE_DIR - the RCPP include directories
+#  RCPP_LIBRARY - link these to use RCPP
 # Autor: Omar Andres Zapata Mesa 31/05/2013
-# Adjusted: Jose Fernandez Navaro
+# Adjusted by: Jose Fernandez Navaro
 
-find_program(R_EXECUTABLE NAMES R R.exe)
+set(RCPP_PKGCONF_LIBRARY_INCLUDE
+    "/usr/local/include"
+    "/usr/include"
+    "/opt/R/site-library/Rcpp/include"
+    "/usr/local/lib/R/site-library/Rcpp/include"
+    "/usr/lib/R/site-library/Rcpp/include"
+    "${RCPP_PATH}/include")
 
-execute_process(COMMAND echo "Rcpp:::CxxFlags()"
-                COMMAND ${R_EXECUTABLE} --vanilla --slave
-                OUTPUT_VARIABLE RCPP_INCLUDE_DIR
-                ERROR_VARIABLE RCPP_INCLUDE_DIR_ERR
-                OUTPUT_STRIP_TRAILING_WHITESPACE)
-  
-execute_process(COMMAND echo "cat(find.package('Rcpp'))"
-                COMMAND ${R_EXECUTABLE} --vanilla --slave
-                OUTPUT_VARIABLE RCPP_PATH
-                ERROR_VARIABLE RCPP_PATH_ERR
-                OUTPUT_STRIP_TRAILING_WHITESPACE)
-
-set(RCPP_PKGCONF_LIBRARY_DIRS  
+set(RCPP_PKGCONF_LIBRARY_DIRS
     "/usr/local/lib"
     "/usr/lib"
     "/opt/R/site-library/Rcpp/lib"
@@ -32,19 +26,19 @@ set(RCPP_PKGCONF_LIBRARY_DIRS
     "${RCPP_PATH}/libs/x64"
     "${RCPP_PATH}/libs/i386")
 
-# Finally the library itself
+# Find the include dir
+find_path(RCPP_INCLUDE_DIR
+  NAMES Rcpp.h
+  PATHS ${RCPP_PKGCONF_LIBRARY_INCLUDE}
+)
+
+# Find the library
 find_library(RCPP_LIBRARY NAMES Rcpp PATHS ${RCPP_PKGCONF_LIBRARY_DIRS})
 
-# Remove un wanted -I from the include path
-string(REPLACE "-I" "" RCPP_INCLUDE_DIR "${RCPP_INCLUDE_DIR}")
-
-# Setting up the results 
-set(RCPP_INCLUDE_DIRS ${RCPP_INCLUDE_DIR})
-set(RCPP_LIBRARIES ${RCPP_LIBRARY})
-if (("${RCPP_INCLUDE_DIR}" STREQUAL "") OR ("${RCPP_LIBRARY}" STREQUAL "")) 
-  set(RCPP_FOUND FALSE)
-  message(STATUS "Looking for Rcpp -- not found ")
-  message(STATUS "Install it running 'R -e \"install.packages(\\\"RInside\\\",repos=\\\"http://cran.irsn.fr/\\\")\"'")
+if(RCPP_INCLUDE_DIR AND RCPP_LIBRARY)
+    set(RCPP_FOUND TRUE)
+    mark_as_advanced(RCPP_LIBRARY RCPP_LIBRARY)
 else()
-  set(RCPP_FOUND TRUE)
+    set(RCPP_FOUND FALSE)
+    message(FATAL_ERROR "Looking for Rcpp -- not found. Try to set RCPP_PATH to the path of Rcpp")
 endif()
