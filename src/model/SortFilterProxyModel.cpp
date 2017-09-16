eefb3f0e593448f91face60fce3697d3a5f5e4ff
jfnavarro <jc.fernandez.navarro@gmail.com>
2017-09-16 15:40:05 +0200

    Fixed a bug in the selection

--- src/model/SortFilterProxyModel.cpp
+++ src/model/SortFilterProxyModel.cpp
@@ -9,9 +9,9 @@ SortFilterProxyModel::SortFilterProxyModel(QObject *parent)
 bool SortFilterProxyModel::lessThan(const QModelIndex &left,
                                     const QModelIndex &right) const
 {
-    const QVariant leftData = sourceModel()->data(left);
-    const QVariant rightData = sourceModel()->data(right);
-    if (leftData.type() == QVariant::Bool) {
+    if (left.column() == 0) {
+        const QVariant leftData = sourceModel()->data(left, Qt::CheckStateRole);
+        const QVariant rightData = sourceModel()->data(right, Qt::CheckStateRole);
         return leftData.toBool() || !rightData.toBool();
     }
     return QSortFilterProxyModel::lessThan(left, right);
