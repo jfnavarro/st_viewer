91e4e4776d29219e2e1b9f4874acb67639e22251
jfnavarro <jc.fernandez.navarro@gmail.com>
2017-11-28 15:31:34 +0100

    Windows building working

--- cmake/FindR.cmake
+++ cmake/FindR.cmake
@@ -22,6 +22,13 @@
 # LIBRINSIDE_LIBRARIES
 # LIBRCPPARMADILLO_INCLUDE_DIRS
 
+    
+# look for the R executable
+find_program(LIBR_EXECUTABLE R)
+if(LIBR_EXECUTABLE-NOTFOUND)
+	message(FATAL_ERROR "Unable to locate R executable")
+endif()
+	
 # detection for OSX (look for R framework)
 if(APPLE)
 
@@ -31,147 +38,101 @@ if(APPLE)
         set(LIBR_INCLUDE_DIRS "${LIBR_HOME}/include" CACHE PATH "R include directory")
         set(LIBR_DOC_DIR "${LIBR_HOME}/doc" CACHE PATH "R doc directory")
         set(LIBR_EXECUTABLE "${LIBR_HOME}/R" CACHE PATH "R executable")
-        set(LIBR_LIBRARIES "${LIBR_HOME}/lib/libR.dylib")
+        set(LIBR_LIB "${LIBR_HOME}/lib/libR.dylib")
     else()
-        message(STATUS "Could not find libR shared library.")
+        message(FATAL_ERROR "Could not find R (OSX)")
     endif()
 
 # detection for UNIX & Win32
 else()
-
-    # Find R executable and paths (UNIX)
-    if(UNIX)
-
-        # find executable
-        find_program(LIBR_EXECUTABLE R)
-        if(LIBR_EXECUTABLE-NOTFOUND)
-            message(STATUS "Unable to locate R executable")
+    # ask R for the home path
+    if(NOT LIBR_HOME)
+        execute_process(
+            COMMAND ${LIBR_EXECUTABLE} "--slave" "--no-save" "-e" "cat(R.home())"
+            OUTPUT_VARIABLE LIBR_HOME
+            )
+        if(LIBR_HOME)
+            set(LIBR_HOME ${LIBR_HOME} CACHE PATH "R home directory")
+		else()
+			message(FATAL_ERROR "Unable to locate R home (Linux)")
         endif()
-
-        # ask R for the home path
-        if(NOT LIBR_HOME)
-            execute_process(
-                COMMAND ${LIBR_EXECUTABLE} "--slave" "--no-save" "-e" "cat(R.home())"
-                OUTPUT_VARIABLE LIBR_HOME
-                )
-            if(LIBR_HOME)
-                set(LIBR_HOME ${LIBR_HOME} CACHE PATH "R home directory")
-            endif()
-        endif()
-
-        # ask R for the include dir
-        if(NOT LIBR_INCLUDE_DIRS)
-            execute_process(
-                COMMAND ${LIBR_EXECUTABLE} "--slave" "--no-save" "-e" "cat(R.home('include'))"
-                OUTPUT_VARIABLE LIBR_INCLUDE_DIRS
-                )
-            if(LIBR_INCLUDE_DIRS)
-                set(LIBR_INCLUDE_DIRS ${LIBR_INCLUDE_DIRS} CACHE PATH "R include directory")
-            endif()
-        endif()
-
-        # ask R for the doc dir
-        if(NOT LIBR_DOC_DIR)
-            execute_process(
-                COMMAND ${LIBR_EXECUTABLE} "--slave" "--no-save" "-e" "cat(R.home('doc'))"
-                OUTPUT_VARIABLE LIBR_DOC_DIR
-                )
-            if(LIBR_DOC_DIR)
-                set(LIBR_DOC_DIR ${LIBR_DOC_DIR} CACHE PATH "R doc directory")
-            endif()
-        endif()
-
-        # ask R for the lib dir
-        if(NOT LIBR_LIB_DIR)
-            execute_process(
-                COMMAND ${LIBR_EXECUTABLE} "--slave" "--no-save" "-e" "cat(R.home('lib'))"
-                OUTPUT_VARIABLE LIBR_LIB_DIR
-                )
-        endif()
-
-    # Find R executable and paths (Win32)
-    else()
-
-        # find the home path
-        if(NOT LIBR_HOME)
-
-            # read home from the registry
-            get_filename_component(LIBR_HOME
-                "[HKEY_LOCAL_MACHINE\\SOFTWARE\\R-core\\R;InstallPath]"
-                ABSOLUTE CACHE)
-
-            # print message if not found
-            if(NOT LIBR_HOME)
-                message(STATUS "Unable to locate R home (not written to registry)")
-            endif()
-
-        endif()
-
-        # set other R paths based on home path
-        set(LIBR_INCLUDE_DIRS "${LIBR_HOME}/include" CACHE PATH "R include directory")
-        set(LIBR_DOC_DIR "${LIBR_HOME}/doc" CACHE PATH "R doc directory")
-
-        # set library hint path based on whether  we are doing a special session 64 build
-        if(LIBR_FIND_WINDOWS_64BIT)
-            set(LIBRARY_ARCH_HINT_PATH "${LIBR_HOME}/bin/x64")
-        else()
-            set(LIBRARY_ARCH_HINT_PATH "${LIBR_HOME}/bin/i386")
-        endif()
-
     endif()
 
-    # look for the R executable
-    find_program(LIBR_EXECUTABLE R
-        HINTS ${LIBRARY_ARCH_HINT_PATH} ${LIBR_HOME}/bin)
-    if(LIBR_EXECUTABLE-NOTFOUND)
-        message(STATUS "Unable to locate R executable")
+    # ask R for the include dir
+    if(NOT LIBR_INCLUDE_DIRS)
+        execute_process(
+			COMMAND ${LIBR_EXECUTABLE} "--slave" "--no-save" "-e" "cat(R.home('include'))"
+            OUTPUT_VARIABLE LIBR_INCLUDE_DIRS
+            )
+        if(LIBR_INCLUDE_DIRS)
+            set(LIBR_INCLUDE_DIRS ${LIBR_INCLUDE_DIRS} CACHE PATH "R include directory")
+        endif()
     endif()
+
+	# ask R for the doc dir
+	if(NOT LIBR_DOC_DIR)
+		execute_process(
+			COMMAND ${LIBR_EXECUTABLE} "--slave" "--no-save" "-e" "cat(R.home('doc'))"
+			OUTPUT_VARIABLE LIBR_DOC_DIR
+			)
+		if(LIBR_DOC_DIR)
+			set(LIBR_DOC_DIR ${LIBR_DOC_DIR} CACHE PATH "R doc directory")
+		endif()
+	endif()
+
+	# ask R for the lib dir
+	if(NOT LIBR_LIB_DIR)
+		execute_process(
+			COMMAND ${LIBR_EXECUTABLE} "--slave" "--no-save" "-e" "cat(R.home('lib'))"
+			OUTPUT_VARIABLE LIBR_LIB_DIR
+			)
+	endif()
 
     # look for the core R library
-    find_library(LIBR_LIB_DIR NAMES R libR
-        HINTS ${LIBR_LIB_DIR} ${LIBRARY_ARCH_HINT_PATH} ${LIBR_HOME} ${LIBR_HOME}/lib ${LIBR_HOME}/bin)
+    find_library(LIBR_LIB NAMES R libR
+        HINTS ${LIBR_LIB_DIR} ${LIBR_HOME}/bin/i386 
+		${LIBR_HOME} ${LIBR_HOME}/lib ${LIBR_HOME}/bin)
 
 endif()
 
-if(LIBR_INCLUDE_DIRS)
+if (LIBR_INCLUDE_DIRS)
     message(STATUS "Found R: ${LIBR_INCLUDE_DIRS}")
 elseif(LIBR_INCLUDE_DIRS)
     message(FATA_ERROR "R could not be found")
 endif()
 
-if(LIBR_LIB_DIR)
-    message(STATUS "Found R libraries: ${LIBR_LIB_DIR}")
-elseif(LIBR_INCLUDE_DIRS)
+if (LIBR_LIB)
+    message(STATUS "Found R libraries: ${LIBR_LIB}")
+elseif(LIBR_LIB)
     message(FATA_ERROR "R libraries could not be found")
 endif()
 
 # look for lapack
 find_library(LIBR_LAPACK_LIBRARY NAMES Rlapack
-    HINTS ${LIBR_LIB_DIR} ${LIBRARY_ARCH_HINT_PATH} ${LIBR_HOME} ${LIBR_HOME}/lib ${LIBR_HOME}/bin)
+	HINTS ${LIBR_LIB_DIR} ${LIBR_HOME}/bin/i386 ${LIBR_HOME} ${LIBR_HOME}/lib ${LIBR_HOME}/bin)
 
-if(LIBR_LAPACK_LIBRARY)
+# look for blas
+find_library(LIBR_BLAS_LIBRARY NAMES Rblas blas
+	HINTS ${LIBR_LIB_DIR} ${LIBR_HOME}/bin/i386 ${LIBR_HOME} ${LIBR_HOME}/lib ${LIBR_HOME}/bin)
+
+if (LIBR_LAPACK_LIBRARY)
     message(STATUS "Found R lapack libraries: ${LIBR_LAPACK_LIBRARY}")
 elseif(LIBR_LAPACK_LIBRARY)
     message(FATA_ERROR "R lapack libraries could not be found")
 endif()
 
-# look for blas
-find_library(LIBR_BLAS_LIBRARY NAMES Rblas blas
-    HINTS ${LIBR_LIB_DIR} ${LIBRARY_ARCH_HINT_PATH} ${LIBR_HOME} ${LIBR_HOME}/lib ${LIBR_HOME}/bin)
-
-if(LIBR_BLAS_LIBRARY)
+if (LIBR_BLAS_LIBRARY)
     message(STATUS "Found R blas libraries: ${LIBR_BLAS_LIBRARY}")
-elseif(LIBR_BLAS_LIBRARY)
+elseif (LIBR_BLAS_LIBRARY)
     message(FATA_ERROR "R blas libraries could not be found")
 endif()
 
-set(LIBR_LIBRARIES ${LIBR_LIBRARIES} ${LIBR_BLAS_LIBRARY} ${LIBR_LAPACK_LIBRARY})
+set(LIBR_LIBRARIES ${LIBR_LIB} ${LIBR_BLAS_LIBRARY} ${LIBR_LAPACK_LIBRARY})
 
 set(NUM_TRUNC_CHARS 2)
 
 execute_process(COMMAND ${LIBR_EXECUTABLE} "--slave" "--no-save" "-e" "Rcpp:::CxxFlags()" OUTPUT_VARIABLE LIBRCPP_INCLUDE_DIRS)
 string(SUBSTRING ${LIBRCPP_INCLUDE_DIRS} ${NUM_TRUNC_CHARS} -1 LIBRCPP_INCLUDE_DIRS)
-
 execute_process(COMMAND ${LIBR_EXECUTABLE} "--slave" "--no-save" "-e" "RInside:::CxxFlags()" OUTPUT_VARIABLE LIBRINSIDE_INCLUDE_DIRS)
 string(SUBSTRING ${LIBRINSIDE_INCLUDE_DIRS} ${NUM_TRUNC_CHARS} -1 LIBRINSIDE_INCLUDE_DIRS)
 execute_process(COMMAND ${LIBR_EXECUTABLE} "--slave" "--no-save" "-e" "RInside:::LdFlags()" OUTPUT_VARIABLE LIBRINSIDE_LIBRARIES)
@@ -187,7 +148,6 @@ if (WIN32)
     string(LENGTH ${LIBRINSIDE_LIBRARIES} lenRInsideFQNameLen)
     math(EXPR lenRInsideFQNameLen ${lenRInsideFQNameLen}-1)
     string(SUBSTRING ${LIBRINSIDE_LIBRARIES} 0 ${lenRInsideFQNameLen} LIBRINSIDE_LIBRARIES)
-	# Replace .a for .dll
 	string(REGEX REPLACE "[.][a]" ".dll" LIBRINSIDE_LIBRARIES ${LIBRINSIDE_LIBRARIES})
 elseif (UNIX AND NOT APPLE)
     if (${LIBRINSIDE_LIBRARIES} MATCHES "[-][L]([^ ;])+")
